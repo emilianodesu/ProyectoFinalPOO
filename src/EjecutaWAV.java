f6079501f53c81ef21d6e7fba6ac9daeb156a30d
Emiliano Mendoza <cemh.fi@comunidad.unam.mx>
2022-12-30 18:20:33 -0600

    Se agrego la lectura del archivo

--- src/EjecutaWAV.java
+++ src/EjecutaWAV.java
@@ -1,8 +1,24 @@
-// import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class EjecutaWAV {
-    public static void main(String[] args) throws IllegalArgumentException, NullPointerException{
-        //Codigo
+    public static void main(String[] args) throws IllegalArgumentException, NullPointerException {
         // Se maneja lectura de archivo con parametros para enviarlo a GeneraWAV
+        try {
+            FileReader fr = new FileReader("Prueba.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String name = br.readLine();
+            int iFrecuenciaMuestreo = Integer.parseInt(br.readLine());
+            int numCanales = Integer.parseInt(br.readLine());
+            int armonico = Integer.parseInt(br.readLine());
+            int iTiempo = Integer.parseInt(br.readLine());
+            br.close();
+            GeneraWAV generador = new GeneraWAV(numCanales);
+            generador.Escribe(name, iTiempo, iFrecuenciaMuestreo, armonico);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        // Se establece valor de armonico de 326767
     }
 }
--- src/GeneraWAV.java
+++ src/GeneraWAV.java
@@ -1,6 +1,16 @@
 public class GeneraWAV {
-    
+
+    private int numCanales;
+
+    public GeneraWAV(int numCanales) {
+        this.numCanales = numCanales;
+    }
+
     public void Escribe(String name, int iTiempo, int iFrecuenciaMuestreo, int armonico) {
-        // Codigo
+        System.out.println(numCanales);
+        System.out.println(name);
+        System.out.println(iTiempo);
+        System.out.println(iFrecuenciaMuestreo);
+        System.out.println(armonico);
     }
 }
