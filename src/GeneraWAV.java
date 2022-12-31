050eab15dd5f981c05dfabc9a50090b5e59289c7
Emiliano Mendoza <cemh.fi@comunidad.unam.mx>
2022-12-30 20:19:12 -0600

    Creacion de archivo wav y modo de escritura

--- src/GeneraWAV.java
+++ src/GeneraWAV.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
 public class GeneraWAV {
 
     private short numCanales;
@@ -6,11 +12,20 @@ public class GeneraWAV {
         this.numCanales = numCanales;
     }
 
-    public void Escribe(String name, int iTiempo, int iFrecuenciaMuestreo, int armonico) {
-        System.out.println(numCanales);
-        System.out.println(name);
-        System.out.println(iTiempo);
-        System.out.println(iFrecuenciaMuestreo);
-        System.out.println(armonico);
+    public void Escribe(String name, int iTiempo, int iFrecuenciaMuestreo, int armonico) throws IOException{
+        try {
+            File archivo = new File(name);
+            archivo.createNewFile();
+            FileWriter fw = new FileWriter(name);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter salida = new PrintWriter(bw);
+            // Escritura
+            salida.println(numCanales);
+            // ...
+            salida.close();
+            
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 }
